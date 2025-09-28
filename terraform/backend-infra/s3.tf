bd246859c44277d248cea71b5621b94c56dc5459
ngel rojas <aomarrojasp@gmail.com>
2025-09-27 21:39:55 -0300

    update indentation

--- terraform/backend-infra/s3.tf
+++ terraform/backend-infra/s3.tf
@@ -9,6 +9,7 @@ resource "aws_s3_bucket" "terraform_state" {
 
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
+
   versioning_configuration {
     status = "Enabled"
   }
@@ -16,17 +17,18 @@ resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
   bucket = aws_s3_bucket.terraform_state.id
-    rule {
-        apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-        }
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "terraform_state_public_access" {
-  bucket = aws_s3_bucket.terraform_state.id
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
--- terraform/backend-infra/variables.tf
+++ terraform/backend-infra/variables.tf
@@ -1,30 +1,35 @@
 variable "aws_region" {
-    description = "The AWS region to create resources in."
-    type        = string
-    default     = "us-east-1"
+  description = "The AWS region to create resources in."
+  type        = string
+  default     = "us-east-1"
 }
+
 variable "environment" {
-    description = "The environment to create resources in."
-    type        = string
-    default     = "prod"
+  description = "The environment to create resources in."
+  type        = string
+  default     = "prod"
 }
+
 variable "project_name" {
-    description = "Project fast api jwt."
-    type        = string
-    default     = "fast-api-jwt"
+  description = "Project fast api jwt."
+  type        = string
+  default     = "fast-api-jwt"
 }
+
 variable "s3_bucket_name" {
-    description = "The name of the S3 bucket to store the Terraform state file."
-    type        = string
-    default     = "storage-csv-img-fies-us-east-1"
+  description = "The name of the S3 bucket to store the Terraform state file."
+  type        = string
+  default     = "storage-csv-img-fies-us-east-1"
 }
+
 variable "dynamodb_table_name" {
-    description = "The name of the DynamoDB table to store the Terraform state locks."
-    type        = string
-    default     = "terraform-locks-fast-api-jwt"
+  description = "The name of the DynamoDB table to store the Terraform state locks."
+  type        = string
+  default     = "terraform-locks-fast-api-jwt"
 }
+
 variable "enable_point_in_time_recovery" {
-    description = "Enable point in time recovery for the DynamoDB table."
-    type        = bool
-    default     = true
+  description = "Enable point in time recovery for the DynamoDB table."
+  type        = bool
+  default     = true
 }
