bb117a2f6295bd5c702dcb8538f177a8d9b48b41
ngel rojas <aomarrojasp@gmail.com>
2025-09-27 21:56:38 -0300

    change the name for dynamodb and s3

--- terraform/backend-infra/variables.tf
+++ terraform/backend-infra/variables.tf
@@ -19,13 +19,13 @@ variable "project_name" {
 variable "s3_bucket_name" {
   description = "The name of the S3 bucket to store the Terraform state file."
   type        = string
-  default     = "tf-state-lock-bucket-prod"
+  default     = "tf-state-locks-fast-api-jwt"
 }
 
 variable "dynamodb_table_name" {
   description = "The name of the DynamoDB table to store the Terraform state locks."
   type        = string
-  default     = "terraform-locks-fast-api-jwt"
+  default     = "tf-table-locks-fast-api-jwt"
 }
 
 variable "enable_point_in_time_recovery" {
