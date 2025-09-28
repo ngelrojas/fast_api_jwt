3d4ad2e20866553d73c19fcd9c180bbb06c6a5b2
ngel rojas <aomarrojasp@gmail.com>
2025-09-28 17:01:38 -0300

    adding more modules to tf files

--- terraform/production/iam/main.tf
+++ terraform/production/iam/main.tf
@@ -1,34 +1,37 @@
 resource "aws_iam_role" "ec2_ssm_role" {
   name = var.ec2_ssm_fast_api
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17",
-        Statement = [
-        {
-            Effect = "Allow",
-            Principal = {
-            Service = "ec2.amazonaws.com"
-            },
-            Action = "sts:AssumeRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
         }
-        ]
-    })
-    tags = {
-        Name        = var.ec2_ssm_fast_api_tag
-        Environment = var.environment
-        Purpose     = "ec2-ssm-role"
-    }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+  tags = {
+    Name        = var.ec2_ssm_fast_api_tag
+    Environment = var.environment
+    Purpose     = "ec2-ssm-role"
+  }
 }
+
 resource "aws_iam_role" "self_hosted_runner" {
   name = "self-hosted-github-actions-runner-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 }
 
@@ -53,7 +56,8 @@ resource "aws_iam_role_policy" "self_hosted_runner_policy" {
     ]
   })
 }
+
 resource "aws_iam_instance_profile" "self_hosted_runner_profile" {
   name = "self-hosted-github-actions-runner-profile"
-  role = aws_iam_role.self_hosted_runner
+  role = aws_iam_role.self_hosted_runner.name
 }
