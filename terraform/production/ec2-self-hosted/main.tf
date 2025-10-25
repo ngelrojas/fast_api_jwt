c9946deb525c3abb1ed19d53a3239abebe71f94b
ngel rojas <aomarrojasp@gmail.com>
2025-10-25 18:32:14 -0300

    update iam for ec2 name change

--- terraform/production/ec2-self-hosted/main.tf
+++ terraform/production/ec2-self-hosted/main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
   filter {
-    name   = "aws-aim-ubuntu-fast-api-jwt"
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 }
